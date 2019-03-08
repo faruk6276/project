096e835431f51686c6db2e2a73c2bf3a1052d970
faruk6276 <31950303+faruk6276@users.noreply.github.com>
2019-03-08 17:59:13 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,158 @@
+<html>
+  <head>
+    <link rel="icon" href="/Project/images/image/tickets.png">
+    <title>Home </title>
+  </head>
+  <body background="/Project/images/image/background1.jpg">
+    <!--Header-->
+   <table width="100%" bgcolor="white">
+     <tr>
+          <td  align="center"><a href="../home/index.php"><img src="/Project/images/image/ticketbari.png" alt="logo" height="60px" width="120px"> </a><br>
+          <font color="#3C896D" size="4"><marquee direction="right">The Home of Tickets</marquee><font></td>
+     </tr>
+      </table>
+      <!--Nav Bar-->
+      <table width="100%" bgcolor="#3C896D">
+        <tr >
+          <td width="10%"></td>
+          <td height="40px" width="10%">
+             <a href="../home/index.php"><img src="/Project/images/icons/home.png" alt=""  height="20px" width="20px"> <font color="#FFFCF9" size="3">Home<font></a>
+          </td>
+          <td height="40px" width="10%">
+             <a href="../cinema/index.php"><img src="/Project/images/icons/cinema.png" alt=""  height="20px" width="20px"><font color="#FFFCF9" size="3">Cinema<font></a>
+          </td>
+          <td height="40px" width="10%">
+             <a href="../bus/index.php"><img src="/Project/images/icons/bus-ticket.png" alt=""  height="20px" width="20px"><font color="#FFFCF9" size="3">Bus<font></a>
+          </td>
+          <td width="20%" align="right"><input type="text" name="search" placeholder="🍳 Search"></td>
+          <td height="40px" width="30%" align="right">
+             <a href="../login/index.php"><img src="/Project/images/icons/signin.png" alt=""  height="20px" width="20px"><font color="#FFFCF9" size="3">Sign In<font></a>
+          </td>
+          <td width="10%"></td>
+        </tr>
+      </table>
+      <br>
+      <br>
+      <br>
+      <br>
+        <!--1st part-->
+      <table width="100%">
+        <tr>
+            <td width="10%"></td>
+            <td width="300px"><img src="/Project/images/image/home1.png" alt=""  height="300px" width="300px"></td>
+        <td width="90%" align="left"> <font color="#FFFCF9" size="5">
+            <h1>Get ticket from home.</h1>
+            <h1>With the best ticket buying <font color="yellow">solution.</font></h1>
+          </font></td>
+        </tr>
+      </table>
+      <!--Running Movie section-->
+      <table bgcolor="#FFFCF9"  width="100%" >
+        <tr>
+
+          <td  colspan="4"align="center"><font color="#454545" size="9">Now showing</font> <br><hr width="200px" align="center"></td>
+        </tr>
+        <tr>
+          <td width="10%"></td>
+          <td><img src="/Project/images/image/cptainmarvel.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">Captain Marvel</font> </h2>
+          <a href="../cinema/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a></td>
+          <td><img src="/Project/images/image/lego2.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">Lego2 Movie</font> </h2>
+        <a href="../cinema/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a>  </td>
+          <td><img src="/Project/images/image/htyg.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">How to train Your Dragon</font> </h2>
+        <a href="../cinema/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a>  </td>
+        </tr>
+      </table>
+      <br>
+      <br>
+      <br>
+      <br>
+      <hr width=800px size="3px" noshade>
+
+      <!--Upcomoing Movie section-->
+      <table width="100%">
+        <tr>
+          <td  colspan="4"align="center"><font color="yellow" size="9">Upcomoing Movies</font> <br><hr width="200px" align="center"></td>
+        </tr>
+        <tr>
+          <td width="10%"></td>
+          <td><img src="/Project/images/image/wonderpark.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#FFFCF9">Wonder Park</font> </h2></td>
+          <td><img src="/Project/images/image/link.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#FFFCF9">Missing link</font> </h2></td>
+          <td><img src="/Project/images/image/shazam.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#FFFCF9">Shazam</font> </h2></td>
+        </tr>
+      </table>
+
+      <br>
+      <hr width=800px size="3px" noshade>
+
+      <!--Bus section-->
+      <table width="100%" bgcolor="#454545">
+        <tr>
+            <td width="10%"></td>
+            <td width="300px"><img src="/Project/images/image/bus1.png" alt=""  height="300px" width="300px"></td>
+        <td width="90%" align="left"> <font color="#FFFCF9" size="5">
+            <h1>Don't have to go the conter to buy ticket?</h1>
+            <h1>Buy bus ticket buying <font color="yellow">solution.</font></h1>
+          </font></td>
+        </tr>
+      </table>
+        <!--Running bus section-->
+      <table  width="100%" >
+        <tr>
+          <td  colspan="4"align="center"><font color="yellow" size="9">Running Buses</font> <br><hr width="200px" align="center"></td>
+        </tr>
+        <tr>
+          <td width="10%"></td>
+          <td><img src="/Project/images/image/ena.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">Ena</font> </h2>
+          <a href="../bus/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a></td>
+          <td><img src="/Project/images/image/hanif.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">Hanif</font> </h2>
+        <a href="../bus/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a>  </td>
+          <td><img src="/Project/images/image/greenline.jpg" alt=""  height="400px" width="250px"><br>
+          <h2><font color="#3C896D">Greenline</font> </h2>
+        <a href="../bus/index.php"><input type="button" name="ticket" value="Get Your Ticket"></a>  </td>
+        </tr>
+      </table>
+
+      <!--Stay with us section-->
+      <table width="100%" bgcolor="#FFFCF9">
+        <tr>
+          <td width="100%" align="center"><img src="/Project/images/image/location.gif" alt=""  height="200x" width="300px"><br>
+           <font color="#454545" size="6">Stay With us.More servies are coming </font></td>
+        </tr>
+      </table>
+     <!--Footer section-->
+     <table width="100%" bgcolor="black">
+       <tr>
+         <td width="10%"></td>
+         <td width="30px" height="100px"><a href="../about/index.php"><font color="#FFFCF9" size="4">About Us</font></a> </td>
+         <td width="30px"> <a href="../contactus/index.php"><font color="#FFFCF9" size="4">Contact us</font></a></td>
+         <td width="30px"><a href="../faq/index.php"><font color="#FFFCF9" size="4">Faq</font></a> </td>
+          <td width="10%"></td>
+          <td width="30px"><font color="#FFFCF9" size="4">Follow Us</font>
+          &nbsp;&nbsp;<a href="https://www.facebook.com/jhon.derby.35"><img src="/Project/images/image/facebook.png" alt=""  height="30x" width="30px"></a>
+        &nbsp;&nbsp;<a href="https://www.twitter.com"><img src="/Project/images/image/twitter.png" alt=""  height="30x" width="30px"></a>
+      &nbsp;&nbsp;<a href="https://www.instagram.com"><img src="/Project/images/image/instagram.png" alt=""  height="30x" width="30px"></a></td>
+         </tr>
+         <tr>
+           <td colspan="6" align="center">
+            <font color="#454545" size="3">@Copy Right Reserved by TicketBari Ltd </font>
+          </td>
+         </tr>
+
+
+     </table>
+
+
+
+
+
+
+  </body>
+</html>
